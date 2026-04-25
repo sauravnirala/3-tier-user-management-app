369a68fec3e68499c1cf51ccc367d4e74fd445ae
sauravnirala <sauravnirala44@gmail.com>
2026-04-25 16:22:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+DATABASE_CONFIG = {
+    "host": os.getenv("DB_HOST", "localhost"),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASS", "root"),
+    "database": os.getenv("DB_NAME", "testdb")
+}
