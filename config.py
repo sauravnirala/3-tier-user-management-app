42dd05583916bfed9a20605a319792218784483b
sauravnirala <sauravnirala44@gmail.com>
2026-04-25 16:48:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DATABASE_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
+    "host": os.getenv("DB_HOST", "db"),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASS", "root"),
     "database": os.getenv("DB_NAME", "testdb")
